3a0cc817515b11e7dda5823674843ec53d75769b
Brandon Harrison <brandonharrisoncode@gmail.com>
2019-05-10 19:04:31 -0500

    Working

--- Extract_Rank/rank_dishes.py
+++ Extract_Rank/rank_dishes.py
@@ -3,17 +3,20 @@ import ujson
 import spacy
 import time
 import jellyfish
-from collections import defaultdict
+import collections
 from textblob import TextBlob
 from pathlib import Path
 
 similarity_threshold = 0.8
 
 def rank_dishes(nlp, reviews):
-    dish_polarities = defaultdict(list)
+    dish_polarities = collections.defaultdict(list)
     for user_id, stars, text in reviews:
         for sentence in text.split('.'):
             doc = nlp(sentence.lower())
+            if not any(ent.label_ == 'DISH' for ent in doc.ents):
+                continue
+            polarity = TextBlob(sentence).sentiment.polarity
             for ent in doc.ents:
                 if ent.label_ == 'DISH':
                     dish_polarities[ent.text.split(' and ')[0].split(' with ')[0]].append(polarity)
@@ -53,20 +56,15 @@ def score(polarities):
 
 if __name__ == '__main__':
     start = time.time()
-
-    max_bytes = 2**31 - 1
-    file_path = 'reviews.json'
-    input_size = os.path.getsize(file_path)
-    with open(file_path, 'r') as f:
-        load = [f.read(max_bytes) for _ in range(0, input_size, max_bytes)]
-        reviews = ujson.loads(''.join(load))
+    with open('emerald_reviews.json', 'r') as f:
+        reviews = ujson.loads(f.read())
 
     mid = time.time()
-    print('Time to load reviews from file:', mid - start)
+    print('Time to load reviews from emerald_reviews.json:', mid - start)
 
-    restaurant_id = 'QXAEGFB4oINsVuTFxEYKFQ'
     nlp = spacy.load(Path('../Dish_Model/models'))
-    dishes = get_dishes(nlp, reviews['QXAEGFB4oINsVuTFxEYKFQ'])
-    print(dishes.most_common(10))
+    dishes = rank_dishes(nlp, reviews)
+    print('Time to extract and rank dishes:', time.time() - mid)
 
-    print('Time to extract dishes from a restaurant:', time.time() - mid)
+    for i in range(20):
+        print(i+1, '-', dishes[i][0], round(dishes[i][1], 3))
